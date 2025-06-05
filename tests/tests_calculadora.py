ce20c46ab7f9f4518db5e6a82d15fa649cad170e
Rodrigo Thadeu <114635356+rodrigothadeu@users.noreply.github.com>
2025-06-05 23:02:49 +0000

    test 4

--- tests/tests_calculadora.py
+++ tests/tests_calculadora.py
@@ -1,4 +1,7 @@
-# tests/test_calculadora.py
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../calculadora')))
 
 import pytest
 from calculadora import somar, subtrair, multiplicar, dividir
